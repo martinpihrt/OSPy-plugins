532d496532f99ac8bf0e982385303e8f112ceb21
Martin Pihrt <martinpihrt@gmail.com>
2018-06-06 14:04:37 +0200

    Add new relay 16 plugin

--- plugins/relay_16/README.md
+++ plugins/relay_16/README.md
@@ -1,1 +1,38 @@
+Direct 16 Relay Outputs Readme
+====
 
+This is plugin is designed for use with Raspberry Pi models with a 40 pin GPIO header and example this board: https://www.tiaowiki.com/w/TIAO_Smart_Sprinkler_Pi_Hardware_Layout. This plugin allows OSPy to control relay boards with up to 16 relays.<br>
+The diagram below shows the GPIO pin header and indicates the pins to use with this plugin.<br>
+You can use a relay board with 1, 2, 4, 8 or 16 relays or a combination of those up to 16 relays.<br>
+<br>
+<b>Caution:</b> when using more than 1 relay board, the boards must all be either low-level trigger or all high-level trigger. You can not mix the trigger types.<br>
+<b>Warning:</b> if you use all 16 outputs, you will not use the UPS Monitor and Air Temperature and Humidity Monitor<br>
+<br>
+Connect the board(s) to the Raspberry Pi following the channel numbering indicated on the wiring diagram. 
+
+The hardware should be connected as follows:
+Possible GPIO [22,24,26,32,36,38,40,21,23,29,31,33,35,37,18,19] Station 1 as HW pin 22, Station 2 as HW pin 24 etc...<br>
+<a href="/plugins/relay_16/static/images/schematics.png"><img src="/plugins/relay_16/static/images/schematics.png" width="100%"></a>
+
+The 5V and ground pins on the Pi can be used to power 1 or 2 relay boards. If more boards are being used or if more than a couple of relays will be turned on at the same time It is a good idea to use a separate power supply to power the boards.<br>
+If you are using more than 8 channels and need to increase the number of stations OSPy displays, open the Options menu > Station Handling and incrase the number of Extension boards. Increasing the number by 1 will add 8 stations to OSPy.<br>
+If the number of relays you are using is not a multiple of 8 you can hide the unused stations. Open the Stations menu and uncheck the boxes under Enabled? next to the stations you are not using.  
+
+Plugin setup
+-----------
+* Check Enable relays:  
+  If checked plugin is enabled.  
+* Relay channels:  
+  Select outputs (1-16).  
+* Trigger Level:  
+  LOW = Station is ON (Output set to 0V), Station OFF (Output set to 3.3V)  
+  HIGH = Station is ON (Output set to 3.3V), Station OFF (Output set to 0V)     
+* Status:  
+  Status window from the plugin.  
+  Example:  
+  Relay 16 plug-in: 2018-06-06 10:35:08 3 Outputs set.  
+  Relay 16 plug-in: Possible GPIO [22,24,26,32,36,38,40,21,23,29,31,33,35,37,18,19].  
+  Relay 16 plug-in: 2018-06-06 10:35:08 Setings Relay Output 22 to HIGH (Station 1 ON).  
+  Relay 16 plug-in: 2018-06-06 10:35:28 Setings Relay Output 22 to LOW  (Station 1 OFF).  
+
+Visit [Martin Pihrt's blog](https://pihrt.com). for more information.  
--- plugins/relay_16/__init__.py
+++ plugins/relay_16/__init__.py
@@ -1,1 +1,210 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Martin Pihrt' 
 
+import datetime
+import time
+
+from threading import Thread, Event
+import traceback
+import json
+
+import web
+from ospy.log import log
+from ospy.options import options
+from ospy.webpages import ProtectedPage
+from ospy.helpers import determine_platform, get_rpi_revision, datetime_string
+from ospy.stations import stations
+from plugins import PluginOptions, plugin_url
+
+import i18n
+
+NAME = 'Direct 16 Relay Outputs'
+LINK = 'settings_page'
+
+plugin_options = PluginOptions(
+    NAME,
+    {
+        'enabled': False,  # default is OFF
+        'relays': 1,       # default 1 relay
+        'active': 'high'   # default is normal logic   
+    })
+
+
+################################################################################
+# Main function loop:                                                          #
+################################################################################
+class Relay16Checker(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self._stop = Event()
+
+        self._sleep_time = 0
+        self.start()
+
+    def stop(self):
+        self._stop.set()
+
+    def update(self):
+        self._sleep_time = 0
+
+    def _sleep(self, secs):
+        self._sleep_time = secs
+        while self._sleep_time > 0 and not self._stop.is_set():
+            time.sleep(1)
+            self._sleep_time -= 1
+
+    def run(self):
+        log.clear(NAME)
+        error_check = False # error signature
+        relay_pins = [0]
+        relay_count = -1
+        msg_debug_err = True
+        msg_debug_on = [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True]
+        msg_debug_off = [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True]
+        time_cleaner = 0
+ 
+        if not plugin_options['enabled']:
+           log.info(NAME, _('Relay 16 plug-in') + ': ' +  _('is disabled.'))
+
+        while not self._stop.is_set():
+            try: 
+                if relay_count != plugin_options['relays'] and plugin_options['enabled']:
+                  relay_count = plugin_options['relays']
+                  log.clear(NAME)
+                  log.info(NAME, _('Relay 16 plug-in') + ': ' + datetime_string() + ' ' + str(plugin_options['relays']) + ' ' + _('Outputs set.')) 
+
+                  #### define the GPIO pins that will be used ####
+                  try:
+                     if determine_platform() == 'pi': # If this will run on Raspberry Pi:
+
+                        import RPi.GPIO as GPIO  # RPi hardware
+                        GPIO.setmode(GPIO.BOARD) #IO channels are identified by header connector pin numbers. Pin numbers are 
+                        
+                        if get_rpi_revision() >= 2: 
+                                     
+                           relay_pins = [22,24,26,32,36,38,40,21,23,29,31,33,35,37,18,19] ### associations for outputs HW connector PINs 
+
+                           log.info(NAME, _('Relay 16 plug-in') + ': ' + _('Possible GPIO pins') + str(relay_pins) + '.') 
+                           for i in range(plugin_options['relays']):   # count 1 or 2, 4, 8, 16 outputs
+                               try:
+                                   GPIO.setup(relay_pins[i], GPIO.OUT) # set pin as outputs
+                               except:
+                                   error_check = True                  # not set pins -> this is error
+                                   log.error(NAME, _('Relay 16 plug-in') + ':\n' + traceback.format_exc())
+                        else:
+                           log.info(NAME, _('Relay 16 plug-in') + ': ' + _('Sorry Raspberry Pi 1 is old version.'))
+                           error_check = True
+                     else:
+                        log.info(NAME, _('Relay 16 plug-in') + ': ' + _('Relay board plugin only supported on Raspberry Pi.'))
+
+                  except:
+                     if msg_debug_err:
+                        log.error(NAME, _('Relay 16 plug-in') + ':\n' + traceback.format_exc())
+                        msg_debug_err = False
+                     error_check = False
+                     pass
+
+                #### plugin 
+                if plugin_options['enabled']:                   # if plugin is enabled
+                  if error_check == False:                      # if not check error
+                    for station in stations.get():              
+                      if station.index+1 <= plugin_options['relays']:  # only if station count is < count relay outputs
+
+                         ### ON
+                         if station.active:                            # stations is on 
+                            if plugin_options['active'] == 'high':     # normal high logic
+                               # relay output on to 3.3V 
+                               GPIO.output(relay_pins[station.index], GPIO.HIGH)
+                               if msg_debug_on[station.index]:
+                                  log.info(NAME, _('Relay 16 plug-in') + ': ' + datetime_string() + ' ' + _('Setings Relay Output') + ' ' + str(relay_pins[station.index]) + ' ' + _('to HIGH') + ' (' + _('Station') + ' ' + str(station.index+1) + ' ' + _('ON') + ').')
+                                  msg_debug_on[station.index] = False
+                                  msg_debug_off[station.index] = True 
+                            else:                                      # inversion low logic
+                               # relay output on to 0V 
+                               GPIO.output(relay_pins[station.index], GPIO.LOW)
+                               if msg_debug_on[station.index]:
+                                  log.info(NAME, _('Relay 16 plug-in') + ': ' + datetime_string() + ' ' + _('Setings Relay Output') + ' ' + str(relay_pins[station.index]) + ' ' + _('to LOW') + ' (' + _('Station') + ' ' + str(station.index+1) + ' ' + _('ON') + ').') 
+                                  msg_debug_on[station.index] = False
+                                  msg_debug_off[station.index] = True
+
+                         ### OFF
+                         else:                                         # stations is off
+                            if plugin_options['active'] == 'high':     # normal high logic
+                               # relay output off to 0V
+                               GPIO.output(relay_pins[station.index], GPIO.LOW)
+                               if msg_debug_off[station.index]:
+                                  log.info(NAME, _('Relay 16 plug-in') + ': ' + datetime_string() + ' ' + _('Setings Relay Output') + ' ' + str(relay_pins[station.index]) + ' ' + _('to LOW') + ' (' + _('Station') + ' ' + str(station.index+1) +  ' ' + _('OFF') + ').')
+                                  msg_debug_off[station.index] = False
+                                  msg_debug_on[station.index] = True
+                            else:                                      # inversion low logic
+                               # relay output off to 3.3V   
+                               GPIO.output(relay_pins[station.index], GPIO.HIGH)
+                               if msg_debug_off[station.index]:
+                                  log.info(NAME, _('Relay 16 plug-in') + ': ' + datetime_string() + ' ' + _('Setings Relay Output') + ' ' + str(relay_pins[station.index]) + ' ' + _('to HIGH') + ' (' + _('Station') + ' ' + str(station.index+1)  + ' ' + _('OFF') + ').')
+                                  msg_debug_off[station.index] = False
+                                  msg_debug_on[station.index] = True
+
+                time.sleep(0.5)
+
+                time_cleaner += 1                 
+
+                if time_cleaner >= 120: # 60 sec timer (ex: 120 * time.sleep(0.5) is 60 sec)
+                   time_cleaner = 0
+                   relay_count = -1
+                   msg_debug_err = True
+                   msg_debug_on = [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True]
+                   msg_debug_off = [True,True,True,True,True,True,True,True,True,True,True,True,True,True,True,True]
+
+              
+
+            except Exception:
+                log.error(NAME, _('Relay 16 plug-in') + ':\n' + traceback.format_exc())
+                msg = -1
+                self._sleep(60)
+
+
+checker = None
+
+
+################################################################################
+# Helper functions:                                                            #
+################################################################################
+  
+def start():
+    global checker
+    if checker is None:
+        checker = Relay16Checker()
+
+
+def stop():
+    global checker
+    if checker is not None:
+        checker.stop()
+        checker.join()
+        checker = None
+
+################################################################################
+# Web pages:                                                                   #
+################################################################################
+class settings_page(ProtectedPage):
+    """Load an html page for entering relay 16 adjustments"""
+
+    def GET(self):
+        return self.plugin_render.relay_16(plugin_options, log.events(NAME))
+
+    def POST(self):
+        plugin_options.web_update(web.input())
+        if checker is not None:
+            checker.update()
+        raise web.seeother(plugin_url(settings_page), True)
+
+
+class settings_json(ProtectedPage):
+    """Returns plugin settings in JSON format"""
+
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+        return json.dumps(plugin_options)
