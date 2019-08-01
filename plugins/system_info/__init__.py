76a97943d3b68bd155f8008c03bac24e526e630f
Martin Pihrt <martinpihrt@seznam.cz>
2019-08-01 15:11:17 +0200

    Add usage and messages for i2c

--- plugins/system_info/__init__.py
+++ plugins/system_info/__init__.py
@@ -50,6 +50,7 @@ def get_overview():
             result.append(_('Network: Unknown'))
         result.append(_('Uptime') + ': ' + helpers.uptime())
         result.append(_('CPU temp') + ': ' + helpers.get_cpu_temp(options.temp_unit) + ' ' + options.temp_unit)
+        result.append(_('CPU usage') + ': ' + str(helpers.get_cpu_usage()) + ' %')
         result.append(_('MAC adress') + ': ' + helpers.get_mac())
         try:
             result.append(_('I2C HEX Adress') + ': ')
@@ -57,11 +58,67 @@ def get_overview():
             cmd = 'sudo i2cdetect -y ' + rev
             result.append(process(cmd))
         except Exception:
-          result.append(_('System info plug-in') + ':\n' + traceback.format_exc())
+            result.append(_('System info plug-in') + ':\n' + traceback.format_exc())
 
-    except:
+        # I2C test for Air Temperature and Humidity Monitor plugin
+        if find_address(0x03):
+            result.append(_('Found ATMEGA328 for Air Temperature and Humidity Monitor plugin (0x03).'))
+
+        # I2C test for Water Tank and Humidity Monitor plugin
+        if find_address(0x04):
+            result.append(_('Found ATMEGA328 for Water Tank and Humidity Monitor plugin (0x04).'))
+
+        # I2C test for Button Control plugin
+        if find_address(0x27):
+            result.append(_('Found MCP23017 for Button Control plugin (0x27).'))
+
+        # I2C test for LCD Display plugin
+        search_range = {addr: 'PCF8574' for addr in range(32, 40)}
+        search_range.update({addr: 'PCF8574A' for addr in range(56, 63)})    
+        if find_address(search_range, range=True):
+            result.append(_('Found PCF8574 for LCD Display plugin (0x20-0x28, 0x38-0x3F).'))
+
+        # I2C test for Wind Speed Monitor/ Water Meter meter plugin
+        search_range = {addr: 'PCF8583' for addr in range(80, 81)}
+        if find_address(search_range, range=True):
+            result.append(_('Found PCF8583 for Wind Speed Monitor or Water Meter plugin (0x50-0x51).'))
+
+        # I2C test for Real Time and NTP time plugin
+        if find_address(0x68):
+            result.append(_('Found DS1307 for Real Time and NTP time plugin (0x68).'))
+        
+
+    except Exception:
         result.append(_('System info plug-in') + ':\n' + traceback.format_exc())
+
     return result
+
+
+def find_address(search_range, range=False):
+    try:
+        import smbus
+        bus = smbus.SMBus(0 if helpers.get_rpi_revision() == 1 else 1)
+    except ImportError:
+        log.warning(NAME, _('Could not import smbus.'))
+    else:
+        if range:
+           for addr, type in search_range.iteritems():
+               try:
+                   bus.read_byte(addr) 
+                   return True
+                   break
+               except Exception:
+                   pass
+               else:
+                   return False
+        else:
+           try:
+               bus.read_byte(search_range) 
+               return True
+           except Exception:
+               pass
+           else:
+               return False
 
 
 def process(cmd):
