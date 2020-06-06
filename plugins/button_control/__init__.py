940f53b79c7098cdc945d0976dac482642616a40
Martin Pihrt <martinpihrt@seznam.cz>
2020-06-06 14:35:47 +0200

    Fix, thanks ITDPG4

--- plugins/button_control/__init__.py
+++ plugins/button_control/__init__.py
@@ -18,7 +18,7 @@ from ospy.programs import programs
 from ospy.options import options
 from ospy.runonce import run_once
 from ospy import helpers
-from ospy.helpers import datetime_string
+from ospy.helpers import get_rpi_revision, datetime_string, reboot, restart, poweroff
 
 import i18n
 
