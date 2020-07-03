eba9b8892ecefd0ed236a02e8f25aa057dc71157
Martin Pihrt <martinpihrt@gmail.com>
2020-07-03 09:43:35 +0200

    add mqtt slave to mqtt plugin. not 100% (only prepare)

--- plugins/mqtt/__init__.py
+++ plugins/mqtt/__init__.py
@@ -7,7 +7,7 @@ import traceback
 import os
 import subprocess
 
-from datetime import datetime
+import datetime
 from threading import Thread, Event
 
 import web
@@ -17,6 +17,9 @@ from plugins import PluginOptions, plugin_url, plugin_data_dir
 from ospy.webpages import ProtectedPage
 from ospy.options import options
 from ospy.helpers import datetime_string
+from ospy.stations import stations
+from ospy.options import rain_blocks
+from ospy.inputs import inputs
 
 import atexit # For publishing down message
 
@@ -33,7 +36,11 @@ plugin_options = PluginOptions(
      'broker_port': 1883,
      'publish_up_down': 'ospy/system',
      'user_name': '',
-     'user_password': ''
+     'user_password': '',
+     'use_mqtt_secondary': False,
+     'control_topic': 'ospy/system/stations',
+     'first_station': 1,
+     'station_count': 8
      }
 )
 
@@ -41,6 +48,7 @@ _client = None
 _subscriptions = {}
 mqtt = None
 last_status = ''
+flag_connected = 0
 
 ################################################################################
 # Main function:                                                               #
@@ -69,11 +77,10 @@ class Sender(Thread):
 
     def run(self):
         log.clear(NAME) 
-        disable_text = True
+        self._sleep(2)
         if not self._stop.is_set(): 
-            if plugin_options["use_mqtt"]:  
-                disable_text = True   
-                try:
+            if plugin_options["use_mqtt"]:   
+                try: 
                     atexit.register(on_restart)
                     publish_status()
                     self._sleep(1)
@@ -83,13 +90,12 @@ class Sender(Thread):
                     self._sleep(60)
             else:
                 # text on the web if plugin is disabled
-                if disable_text:  
-                    log.clear(NAME)
-                    log.info(NAME, _('MQTT plug-in is disabled.'))
-                    disable_text = False 
-                    self._sleep(1)
+                log.clear(NAME)
+                log.info(NAME, _('MQTT plug-in is disabled.'))
+                on_stop()
+                self._sleep(1)
         else:
-            self._sleep(1)
+            self._sleep(2)
 
 sender = None
 
@@ -119,10 +125,88 @@ def proc_install(cmd):
     log.info(NAME, output)
 
 def on_message(client, userdata, message):
+    log.clear(NAME) 
     log.info(NAME, datetime_string() + ' ' + _('Message received') + ': ' + str(message.payload.decode("utf-8")))
     #print("Message topic=",message.topic)
     #print("Message qos=",message.qos)
     #print("Message retain flag=",message.retain)
+
+    if plugin_options["use_mqtt_secondary"]:
+        if not options.manual_mode:     # check operation status
+            log.info(NAME, datetime_string() + ' ' + _('You must this OSPy switch to manual mode!'))
+            return
+
+        if inputs.rain_sensed():        # check rain sensor
+            log.info(NAME, datetime_string() + ' ' + _('Skipping command, rain sense is detected!'))
+            return        
+
+        if rain_blocks.seconds_left():  # check rain delay
+            log.info(NAME, datetime_string() + ' ' + _('Skipping command, rain delay is activated!'))
+            return             
+
+        try:
+            rec_data = str(message.payload)
+            cmd = json.loads(rec_data)
+        except ValueError as e:
+            log.info(NAME, _('Could not decode command:') + ':\n' + message.payload, e)
+            return
+ 
+        first = int(plugin_options["first_station"])-1       # first secondary station
+        count = int(plugin_options["station_count"])+1       # count secondary station
+
+        try:
+            for i in range(first, count):                    # count of station (example on main OSPy: 5 to 10) 
+                zone    = cmd[i]["status"]                   # "off" or "on" state 
+                #station = cmd[i]["station"] 
+                #name    = cmd[i]["name"] 
+                #print station, name, zone
+
+                sid = i # TODO
+                if sid <= stations.count():                  # local station size check
+                    if zone == "on":
+                        start = datetime.datetime.now()
+                        mqn = _('MQTT Manual')
+                        new_schedule = {
+                            'active': True,
+                            'program': -1,
+                            'station': sid,
+                            'program_name': mqn,
+                            'fixed': True,
+                            'cut_off': 0,
+                            'manual': True,
+                            'blocked': False,
+                            'start': start,
+                            'original_start': start,
+                            'end': start + datetime.timedelta(days=3650),
+                            'uid': '%s-%s-%d' % (str(start), mqn, sid),
+                            'usage': stations.get(sid).usage
+                        }
+                        # if an optional duration time is given
+                        #set_time = xxx second
+                        #new_schedule['end'] = datetime.datetime.now() + datetime.timedelta(seconds=set_time)
+
+                        log.start_run(new_schedule)
+                        stations.activate(new_schedule['station'])
+                    if zone == "off":
+                        stations.deactivate(sid)
+                        active = log.active_runs()
+                        for interval in active:
+                            if interval['station'] == i:
+                                log.finish_run(interval)
+                            
+                else:
+                    log.error(NAME, _('MQTT plug-in') + ':\n' + _('Setup stations count is smaler! Set correct first station and station count.'))   
+        except Exception:
+            log.error(NAME, _('MQTT plug-in') + ':\n' + traceback.format_exc())
+                    
+
+#if station.enabled  in stations.get():
+#for station in stations.get():
+        #station.active
+
+        time.sleep(1)
+            
+        
 
 def get_client():
     if not os.path.exists("/usr/lib/python2.7/dist-packages/pip"):
@@ -147,11 +231,14 @@ def get_client():
     except ImportError:      
         mqtt = None
         log.error(NAME, _('Error try install paho-mqtt manually.'))
+        time.sleep(60)
  
     if mqtt is not None and plugin_options["use_mqtt"]:  
         try:
             _client = mqtt.Client(options.name)                  # Use system name as client ID 
-            _client.on_message = on_message                      # Attach function to callback
+            _client.on_connect = on_connect                      # flag = 1 is connected
+            _client.on_disconnect = on_disconnect                # flag = 0 is disconnected
+            _client.on_message = on_message                      # Attach function to callback            
             log.clear(NAME)
             log.info(NAME, datetime_string() + ' ' + _('Connecting to broker') + '...')
             _client.username_pw_set(plugin_options['user_name'], plugin_options['user_password'])
@@ -164,13 +251,17 @@ def get_client():
             return None
  
 def publish_status(status="UP"):
-    global last_status
+    global last_status, flag_connected
     client = get_client()
-    if client and plugin_options["use_mqtt"]:  
+    time.sleep(2)
+    if client and plugin_options["use_mqtt"]:  # Publish message
         if status != last_status:
             last_status = status  
             log.info(NAME, datetime_string() + ' ' + _('Subscribing to topic') + ': ' + str(plugin_options['publish_up_down']))
             client.subscribe(plugin_options['publish_up_down'])
+            if plugin_options["use_mqtt_secondary"]:
+                log.info(NAME, datetime_string() + ' ' + _('Subscribing to topic') + ': ' + str(plugin_options['control_topic']))    
+                client.subscribe(plugin_options['control_topic'])        
             client.publish(plugin_options['publish_up_down'], status)
 
 def subscribe(topic, callback, qos=0):
@@ -182,18 +273,37 @@ def subscribe(topic, callback, qos=0):
         if topic not in _subscriptions:
             _subscriptions[topic] = [callback]
             client.subscribe(topic, qos)
-            log.info(NAME, datetime_string() + ' ' + _('Subscribe') + ': ' + str(topic))
+            log.info(NAME, datetime_string() + ' ' + _('Subscribe topic') + ': ' + str(topic))
         else:
             _subscriptions[topic].append(callback)    
 
+def on_connect(client, userdata, flags, rc):
+   global flag_connected
+   flag_connected = 1
+   log.info(NAME, datetime_string() + ' ' + _('Connected to broker.'))
+
+def on_disconnect(client, userdata, rc):
+   global flag_connected
+   flag_connected = 0
+   log.error(NAME, datetime_string() + ' ' + _('Disconnected from broker!'))
+
 def on_restart():
     client = get_client()
+    time.sleep(2)
     if client is not None:
         publish_status("DOWN")
         client.disconnect()
         client.loop_stop()
         client = None
         log.info(NAME, datetime_string() + ' ' +  _('Client stop'))
+
+def on_stop():
+    client = get_client()
+    if client is not None:
+        client.disconnect()
+        client.loop_stop()
+        client = None
+        log.info(NAME, datetime_string() + ' ' +  _('Client stop'))        
 
 ################################################################################
 # Web pages:                                                                   #
@@ -209,7 +319,6 @@ class settings_page(ProtectedPage):
         plugin_options.web_update(web.input())
         if sender is not None:
             sender.update()
-        publish_status()
         raise web.seeother(plugin_url(settings_page), True)
 
 class settings_json(ProtectedPage):
