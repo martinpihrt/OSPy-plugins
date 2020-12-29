368549e0d9a377cae3f5dcda86935722042c3b69
Martin Pihrt <martinpihrt@seznam.cz>
2020-12-29 21:52:02 +0100

    Add blue and green color. Add Wind monitor. Add text or canvas mode.

--- plugins/weather_stations/README.md
+++ plugins/weather_stations/README.md
@@ -5,13 +5,18 @@ This plugin Displays data probes DS18B20 (DS1-6) from Air Temperature and Humidi
 
 Plugin setup
 -----------
+* Canvas or Text mode for showing data:  
+  When checked, it is text mode.
 
 * Canvas Size XY:  
-  The number determines the size of the meter wheel. The minimum is 150px, the maximum is 450px.  
+  The number determines the size of the meter wheel. The minimum is 150px, the maximum is 850px.  
+
+* Text Font Size:
+  The number determines the font size in text mode. The minimum is 15px, the maximum is 800px.  
 
 ### This part needs correct setuped plugin Air Temperature and Humidity Monitor
-* Canvas Count DS18B20:  
-  The number determines of meters for air_temp_humi plugin (DS18B20 sensors).  
+* Show this DS1:  
+  When checked, showing this canvas (text) is enabled.    
 
 * Name for Canvas DS1:  
   Any name for this meter.  
@@ -30,27 +35,18 @@ Plugin setup
 
 * Red color from to:  
   Red color on the scale according to the value from to.  
+
+* Blue color from to:  
+  Blue color on the scale according to the value from to. 
+
+* Green color from to:  
+  Green color on the scale according to the value from to.    
+
+If the set range is the same (for example from 0 to 0) then the color will not be displayed.  
   
 ### This part needs correct setuped plugin Water Tank Monitor        
-* Name for Canvas Water Percent:  
-  Any name for this meter.  
-
-* Name for Canvas Water Volume:  
-  Any name for this meter.      
-
-* Units:  
-  In which units we want to display the data.  
-
-* Scale:  
-  Division scale according to our requirements.  
-
-* Min:  
-  This value must be the minimum of the range from "Scale".  
-
-* Max:  
-  This value must be the maximum of the range from "Scale".  
-
-* Red color from to:  
-  Red color on the scale according to the value from to. 
-
+  Same setting as in temperature.
+ 
+### This part needs correct setuped plugin Wind Speed Monitor
+  Same setting as in temperature.
 
--- plugins/weather_stations/__init__.py
+++ plugins/weather_stations/__init__.py
@@ -16,8 +16,19 @@ LINK = 'canvas_page'
 plugin_options = PluginOptions(
     NAME,
     {
+    'can_or_txt': False,                                            # canvas or text mode showing data
     'can_size_xy': 250,                                             # all canvas x and y size in pixels
-    'can_temperature_count': 6,                                     # canvas temperature count 1-6 (for air temp humi plugin)
+    'txt_size_font': 40,                                            # size for font in text mode    
+
+    'use_can1': True,                                               # use canvas 1 or text 1
+    'use_can2': True,
+    'use_can3': True,
+    'use_can4': True,
+    'use_can5': True,
+    'use_can6': True,
+    'use_can7': True,
+    'use_can8': True,
+    'use_can9': True,
 
     'can_1_units': _(u'°C'),                                        # canvas 1 units °C or ...
     'can_2_units': _(u'°C'),
@@ -27,6 +38,7 @@ plugin_options = PluginOptions(
     'can_6_units': _(u'°C'),  
     'can_7_units': _(u'%'),
     'can_8_units': _(u'liters'),
+    'can_9_units': _(u'm/s'),
                 
     'can_1_name': _(u'Canvas DS1'),                                 # canvas 1 name
     'can_2_name': _(u'Canvas DS2'),
@@ -35,7 +47,8 @@ plugin_options = PluginOptions(
     'can_5_name': _(u'Canvas DS5'),
     'can_6_name': _(u'Canvas DS6'), 
     'can_7_name': _(u'Percent'),
-    'can_8_name': _(u'Volume'),                 
+    'can_8_name': _(u'Volume'), 
+    'can_9_name': _(u'Wind'),                
 
     'can_1_tick': '-10,0,10,20,30,40,50,60',                        # canvas 1 tick scale division
     'can_2_tick': '0,10,20,30,40,50,60,70,80,90,100,110,130',
@@ -45,6 +58,7 @@ plugin_options = PluginOptions(
     'can_6_tick': '0,10,20,30,40,50,60,70,80,90,100,110,130',
     'can_7_tick': '0,10,20,30,40,50,60,70,80,90,100,110,130',
     'can_8_tick': '0,10,20,30,40,50,60,70,80,90,100,110,130',
+    'can_9_tick': '0,10,20,30,40,50',
 
     'can_1_min' : '-10',                                            # canvas 1 minimum value for highlights
     'can_1_max' : '60',                                             # canvas 1 maximum value for highlights
@@ -62,23 +76,63 @@ plugin_options = PluginOptions(
     'can_7_max' : '130',
     'can_8_min' : '0',
     'can_8_max' : '130',
+    'can_9_min' : '0',
+    'can_9_max' : '50',    
 
     'can_1_high_fr': 0,                                             # red color on highlights from 
     'can_1_high_to': 15,                                            # red color on highlights to
+    'can_1_2_high_fr': 15,                                          # blue color on highlights from 
+    'can_1_2_high_to': 30,                                          # blue color on highlights to
+    'can_1_3_high_fr': 30,                                          # green color on highlights from 
+    'can_1_3_high_to': 45,                                          # green color on highlights to        
     'can_2_high_fr': 5,  
     'can_2_high_to': 25,
+    'can_2_2_high_fr': 15,                                          
+    'can_2_2_high_to': 30,                                          
+    'can_2_3_high_fr': 30,                                          
+    'can_2_3_high_to': 45,                                              
     'can_3_high_fr': 30,  
     'can_3_high_to': 40,
+    'can_3_2_high_fr': 15,                                          
+    'can_3_2_high_to': 30,                                          
+    'can_3_3_high_fr': 30,
+    'can_3_3_high_to': 45,                                              
     'can_4_high_fr': 50,  
     'can_4_high_to': 60,
+    'can_4_2_high_fr': 15,                                           
+    'can_4_2_high_to': 30,                                          
+    'can_4_3_high_fr': 30,                                          
+    'can_4_3_high_to': 45,                                             
     'can_5_high_fr': 70,  
     'can_5_high_to': 80,
+    'can_5_2_high_fr': 15,                                           
+    'can_5_2_high_to': 30,                                          
+    'can_5_3_high_fr': 30,                                          
+    'can_5_3_high_to': 45,                                             
     'can_6_high_fr': 90,  
     'can_6_high_to': 100, 
+    'can_6_2_high_fr': 15,                                           
+    'can_6_2_high_to': 30,                                          
+    'can_6_3_high_fr': 30,                                          
+    'can_6_3_high_to': 45,                                              
     'can_7_high_fr': 70,  
     'can_7_high_to': 80,
+    'can_7_2_high_fr': 15,                                          
+    'can_7_2_high_to': 30,                                          
+    'can_7_3_high_fr': 30,                                           
+    'can_7_3_high_to': 45,                                              
     'can_8_high_fr': 90,  
-    'can_8_high_to': 100,                   
+    'can_8_high_to': 100,           
+    'can_8_2_high_fr': 15,                                           
+    'can_8_2_high_to': 30,                                          
+    'can_8_3_high_fr': 30,                                           
+    'can_8_3_high_to': 45,  
+    'can_9_high_fr': 10,  
+    'can_9_high_to': 50,           
+    'can_9_2_high_fr': 0,                                           
+    'can_9_2_high_to': 0,                                          
+    'can_9_3_high_fr': 0,                                           
+    'can_9_3_high_to': 0,                                            
     }
 )
 
@@ -141,6 +195,12 @@ class data_json(ProtectedPage):
             data['temp_ds5'] = air_temp_humi.DS18B20_read_probe(5)
         except: 
             #log.error(NAME, traceback.format_exc())
+            data['temp_ds0'] = -127
+            data['temp_ds1'] = -127
+            data['temp_ds2'] = -127
+            data['temp_ds3'] = -127
+            data['temp_ds4'] = -127
+            data['temp_ds5'] = -127
             pass
 
         try:    
@@ -149,6 +209,16 @@ class data_json(ProtectedPage):
             data['water_2'] = tank_monitor.get_all_values()[3]
         except: 
             #log.error(NAME, traceback.format_exc())
-            pass            
+            data['water_1'] = -127
+            data['water_2'] = -127
+            pass  
+
+        try:    
+            from plugins import wind_monitor
+            data['wind_1'] = wind_monitor.get_all_values()[0]
+        except: 
+            #log.error(NAME, traceback.format_exc())
+            data['wind_1'] = -127
+            pass                      
 
         return json.dumps(data) 
