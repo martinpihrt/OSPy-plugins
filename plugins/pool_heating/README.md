1419794c83a8478ba6309e948180532a60fb5e5c
Martin Pihrt <martinpihrt@seznam.cz>
2021-03-15 13:41:52 +0100

    Updating Slovak language (Thank you Tomáš Szepesi). Add to plugin "pool heating" possibility to read temperatures from OSPy sensors.

--- plugins/pool_heating/README.md
+++ plugins/pool_heating/README.md
@@ -1,7 +1,7 @@
 Pool Heating Readme
 ====
 
-This plugin needs enabled and corect setup  the "Air Temperature and Humidity Monitor". Allows pool heating regulation. Reading temperature using from DS18B20 sensors connected to the external hardware board via an I2C bus (address 0x03). 
+This plugin needs enabled and corect setup the "Air Temperature and Humidity Monitor". Allows pool heating regulation. Reading temperature using from DS18B20 sensors connected to the external hardware board via an I2C bus (address 0x03). Or it is possible to use temperature measurement from OSPy sensors (LAN / Wi-Fi, radio...)
 Example: for heating a swimming pool with a solar panel. If the temperature difference (solar panel and pool water) is higher, the output (filter pump) is switched on. If the temperature difference from the solar panel is lower than the set temperature, the output (filter pump) is switched off.
 Visit [Martin Pihrt's blog](https://pihrt.com/elektronika/380-moje-raspberry-pi-plugin-ospy-mereni-teploty-pomoci-ds18b20). for more information for HW.
 
@@ -9,7 +9,10 @@ Plugin setup
 -----------
 
 * Check Enable Control:  
-  If checked Enable Control, control for regulation is enabled.
+  If checked Enable Control, control for regulation is enabled.  
+
+* Sensors or plugin:  
+  Select the temperature probes from sensors or from plugin air temperature.  
 
 * Probe input from swimming pool: 
   Select the temperature sensor (its name) you want to use.
--- plugins/pool_heating/__init__.py
+++ plugins/pool_heating/__init__.py
@@ -10,6 +10,7 @@ from threading import Thread, Event
 
 import web
 
+from ospy.options import options
 from ospy.log import log
 from plugins import PluginOptions, plugin_url, plugin_data_dir
 from ospy.webpages import ProtectedPage
@@ -17,6 +18,7 @@ from ospy.helpers import get_rpi_revision
 from ospy.helpers import datetime_string
 from ospy import helpers
 from ospy.stations import stations
+from ospy.sensors import sensors
 
 from ospy.webpages import showInFooter # Enable plugin to display readings in UI footer
 #from ospy.webpages import showOnTimeline # Enable plugin to display station data on timeline
@@ -43,7 +45,10 @@ plugin_options = PluginOptions(
      'ds_name_5': '',      # name for DS probe 6 from air temp humi plugin
      'ds_count': 0,        # DS probe count from air temp humi plugin
      'reg_mm': 60,         # min for maximal runtime
-     'reg_ss': 0           # sec for maximal runtime     
+     'reg_ss': 0,          # sec for maximal runtime
+     'probe_A_on_sens': 0, # for selector from sensors xx - temperature ON
+     'probe_A_off_sens': 0,# for selector from sensors xx - temperature OFF
+     'sensor_probe': 0     # selector for type probes: 0=none, 1=sensors, 2=air temp plugin
      }
 )
 
@@ -84,40 +89,83 @@ class Sender(Thread):
         temp_sw.button = "pool_heating/settings"   # button redirect on footer
         temp_sw.label =  _(u'Pool Heating')        # label on footer
 
-        millis = int(round(time.time() * 1000))          # timer for clearing status on the web pages after 60 sec
-        last_millis = millis
+        ds_a_on  = -127.0
+        ds_a_off = -127.0
 
-        a_state = -1                                     # for state in footer (-1 disable regulation A, 0 = Aoff, 1 = Aon)
+        millis = 0                                 # timer for clearing status on the web pages after 5 sec
+        last_millis = int(round(time.time() * 1000))
+
+        a_state = -3                               # for state in footer "Waiting."
         regulation_text = _(u'Waiting to turned on or off.')
+        
+        if not plugin_options['enabled_a']:
+            a_state = -1                           # for state in footer "Waiting (not enabled regulation in options)."
 
-        log.info(NAME, datetime_string() + ' ' + _(u'Waiting to turned on or off.'))
+        log.info(NAME, datetime_string() + ' ' + _(u'Waiting.'))
 
         while not self._stop.is_set():
             try:
-                try:
-                    from plugins.air_temp_humi import plugin_options as air_temp_data
-                    plugin_options['ds_name_0'] = air_temp_data['label_ds0']
-                    plugin_options['ds_name_1'] = air_temp_data['label_ds1']
-                    plugin_options['ds_name_2'] = air_temp_data['label_ds2']
-                    plugin_options['ds_name_3'] = air_temp_data['label_ds3']
-                    plugin_options['ds_name_4'] = air_temp_data['label_ds4']
-                    plugin_options['ds_name_5'] = air_temp_data['label_ds5']
-                    plugin_options['ds_count'] = air_temp_data['ds_used']
+                if plugin_options["sensor_probe"] == 2:                                        # loading probe name from plugin air_temp_humi
+                    try:
+                        from plugins.air_temp_humi import plugin_options as air_temp_data
+                        plugin_options['ds_name_0'] = air_temp_data['label_ds0']
+                        plugin_options['ds_name_1'] = air_temp_data['label_ds1']
+                        plugin_options['ds_name_2'] = air_temp_data['label_ds2']
+                        plugin_options['ds_name_3'] = air_temp_data['label_ds3']
+                        plugin_options['ds_name_4'] = air_temp_data['label_ds4']
+                        plugin_options['ds_name_5'] = air_temp_data['label_ds5']
+                        plugin_options['ds_count'] = air_temp_data['ds_used']
 
-                    from plugins.air_temp_humi import DS18B20_read_probe
-                    temperature_ds = [DS18B20_read_probe(0), DS18B20_read_probe(1), DS18B20_read_probe(2), DS18B20_read_probe(3), DS18B20_read_probe(4), DS18B20_read_probe(5)]
+                        from plugins.air_temp_humi import DS18B20_read_probe                    # value with temperature from probe DS1-DS6
+                        temperature_ds = [DS18B20_read_probe(0), DS18B20_read_probe(1), DS18B20_read_probe(2), DS18B20_read_probe(3), DS18B20_read_probe(4), DS18B20_read_probe(5)]
                     
-                except:
-                    log.error(NAME, _(u'Unable to load settings from Air Temperature and Humidity Monitor plugin! Is the plugin Air Temperature and Humidity Monitor installed and set up?'))
-                    self._sleep(60)
+                    except:
+                        log.error(NAME, _(u'Unable to load settings from Air Temperature and Humidity Monitor plugin! Is the plugin Air Temperature and Humidity Monitor installed and set up?'))
+                        pass
 
                 # regulation
-                if plugin_options['enabled_a']:  
-                    ds_a_on = temperature_ds[plugin_options['probe_A_on']]    #  pool  
-                    ds_a_off = temperature_ds[plugin_options['probe_A_off']]  #  solar
+                if plugin_options['enabled_a'] and plugin_options["sensor_probe"] != 0:# enabled regulation and selected input for probes sensor/airtemp plugin
+                    if plugin_options["sensor_probe"] == 1:
+                        sensor_on = sensors.get(int(plugin_options['probe_A_on_sens']))#  pool
+                        if sensor_on.sens_type == 5:                                   # temperature sensor
+                            ds_a_on = sensor_on.last_read_value
+                        elif sensor_on.sens_type == 6 and sensor_on.multi_type == 0:   # multitemperature sensor DS1
+                            ds_a_on = sensor_on.last_read_value[0]
+                        elif sensor_on.sens_type == 6 and sensor_on.multi_type == 1:   # multitemperature sensor DS2
+                            ds_a_on = sensor_on.last_read_value[1]
+                        elif sensor_on.sens_type == 6 and sensor_on.multi_type == 2:   # multitemperature sensor DS3
+                            ds_a_on = sensor_on.last_read_value[2]
+                        elif sensor_on.sens_type == 6 and sensor_on.multi_type == 3:   # multitemperature sensor DS4
+                            ds_a_on = sensor_on.last_read_value[3]
+                        else:
+                            ds_a_on = -127.0
+
+                        sensor_off = sensors.get(int(plugin_options['probe_A_off_sens']))#  solar
+                        if sensor_off.sens_type == 5:                                    # temperature sensor
+                            ds_a_off = sensor_off.last_read_value
+                        elif sensor_off.sens_type == 6 and sensor_off.multi_type == 0:   # multitemperature sensor DS1
+                            ds_a_off = sensor_off.last_read_value[0]
+                        elif sensor_off.sens_type == 6 and sensor_off.multi_type == 1:   # multitemperature sensor DS2
+                            ds_a_off = sensor_off.last_read_value[1]
+                        elif sensor_off.sens_type == 6 and sensor_off.multi_type == 2:   # multitemperature sensor DS3
+                            ds_a_off = sensor_off.last_read_value[2]
+                        elif sensor_off.sens_type == 6 and sensor_off.multi_type == 3:   # multitemperature sensor DS4
+                            ds_a_off = sensor_off.last_read_value[3]
+                        else:
+                            ds_a_off = -127.0
+
+                    elif plugin_options["sensor_probe"] == 2:
+                        ds_a_on = temperature_ds[plugin_options['probe_A_on']]           #  pool  
+                        ds_a_off = temperature_ds[plugin_options['probe_A_off']]         #  solar
+                        
                     station_a = stations.get(plugin_options['control_output_A'])
 
-                    if (ds_a_off - ds_a_on) > plugin_options['temp_a_on']:    # ON
+                    probes_ok = True
+                    if ds_a_on == -127.0 or ds_a_off == -127.0:
+                    	probes_ok = False
+                    	a_state = -2
+
+                    if (ds_a_off - ds_a_on) > plugin_options['temp_a_on'] and probes_ok: # ON
                         a_state = 1
                         if msg_a_on:
                             msg_a_on = False
@@ -146,45 +194,60 @@ class Sender(Thread):
                             }
 
                             log.start_run(new_schedule)
-                            stations.activate(new_schedule['station'])                            
+                            stations.activate(new_schedule['station'])
 
-                    if (ds_a_off - ds_a_on) < plugin_options['temp_a_off']:   # OFF
+                    if (ds_a_off - ds_a_on) < plugin_options['temp_a_off'] and probes_ok: # OFF
                         a_state = 0
                         if msg_a_off:
                             msg_a_off = False
                             msg_a_on = True
                             regulation_text = datetime_string() + ' ' + _(u'Regulation set OFF.') + ' ' + ' (' + _(u'Output') + ' ' +  str(station_a.index+1) + ').'
                             log.clear(NAME)
-                            log.info(NAME, regulation_text)   
+                            log.info(NAME, regulation_text)
                             sid = station_a.index
                             stations.deactivate(sid)
                             active = log.active_runs()
                             for interval in active:
                                 if interval['station'] == sid:
-                                    log.finish_run(interval)  
+                                    log.finish_run(interval)
   
                 else:
-                    a_state = -1    
-
+                    a_state = -1
 
                 # footer text
                 tempText = ' '
 
                 if a_state == 0:
-                    tempText += regulation_text  
+                    tempText += regulation_text
                 if a_state == 1:
-                    tempText += regulation_text      
+                    tempText += regulation_text
                 if a_state == -1:
-                    tempText = _(u'Waiting to turned on or off.')
+                    tempText = _(u'Waiting (not enabled regulation in options, or not selected input).')
+                if a_state == -2:
+                    tempText = _(u'Some probe shows a fault, regulation is blocked!')
+                if a_state == -3:
+                    tempText = _(u'Waiting.')
 
-                temp_sw.val = tempText.encode('utf8')    # value on footer                                                          
+                temp_sw.val = tempText.encode('utf8')    # value on footer
 
                 self._sleep(2)
 
                 millis = int(round(time.time() * 1000))
-                if (millis - last_millis) > 60000:    # 60 second to clearing status on the webpage
+                if (millis - last_millis) > 5000:        # 5 second to clearing status on the webpage
                     last_millis = millis
                     log.clear(NAME)
+                    if plugin_options["sensor_probe"] == 1:
+                        try:
+                            if options.temp_unit == 'C':
+                                log.info(NAME, datetime_string() + '\n' + sensor_on.name + ' (' + _(u'Pool') + u') %.1f \u2103 \n' % ds_a_on + sensor_off.name + ' ('+ _(u'Solar') + u') %.1f \u2103' % ds_a_off)
+                            else:
+                                log.info(NAME, datetime_string() + '\n' + sensor_on.name + ' (' + _(u'Pool') + u') %.1f \u2109 \n' % ds_a_on + sensor_off.name + ' ('+ _(u'Solar') + u') %.1f \u2103' % ds_a_off)
+                        except:
+                            pass
+                    elif plugin_options["sensor_probe"] == 2:
+                        log.info(NAME, datetime_string() + '\n' + _(u'Pool') + u' %.1f \u2103 \n' % ds_a_on + _(u'Solar') + u' %.1f \u2103' % ds_a_off)
+                        
+                    log.info(NAME, tempText)
  
             except Exception:
                 log.error(NAME, _(u'Pool Heating plug-in') + ':\n' + traceback.format_exc())
@@ -217,14 +280,21 @@ class settings_page(ProtectedPage):
     """Load an html page for entering adjustments and deleting logs"""
 
     def GET(self):
+    	plugin_options.web_update(web.input())
+        if sender is not None:
+            sender.update()           
+
         return self.plugin_render.pool_heating(plugin_options, log.events(NAME))
 
     def POST(self):
         plugin_options.web_update(web.input())
 
         if sender is not None:
-            sender.update()                
-        raise web.seeother(plugin_url(settings_page), True)
+            sender.update()
+            log.clear(NAME)
+
+        #raise web.seeother(plugin_url(settings_page), True)
+        return self.plugin_render.pool_heating(plugin_options, log.events(NAME))        
 
 
 class settings_json(ProtectedPage):
