f3fcd63c7d996366e3dc6594ec68e22da8af41fc
Martin Pihrt <martinpihrt@seznam.cz>
2024-03-09 16:26:40 +0100

    Add backup section (manual create backub db and download db backup file)

--- plugins/database_connector/__init__.py
+++ plugins/database_connector/__init__.py
@@ -8,6 +8,8 @@ import json
 import traceback
 import time
 import subprocess
+import os
+import mimetypes
 
 from threading import Thread, Event
 
@@ -32,7 +34,9 @@ plugin_options = PluginOptions(
         'user': 'username',
         'pass': 'password',
         'port':  3306,
-        'database': 'ospy'
+        'database': 'ospy',
+        'sql_name': [],                # a list of all sql names in the plugin data directory
+        'sql_size': [],                # sql size in bytes        
     }
 )
 
@@ -71,15 +75,19 @@ class Sender(Thread):
                     try:
                         import mariadb
                         log.info(NAME, _('Mariadb installed - OK.'))
-                        maria_installed_ok = True             
+                        maria_installed_ok = True
                     except ImportError:
                         log.info(NAME, _('Mariadb is not installed.'))
                         log.info(NAME, _('Please wait installing mariadb...'))
                         cmd = "sudo apt-get install -y libmariadb-dev"
                         run_command(cmd)
                         cmd = "sudo apt-get install libmariadb3"
-                        run_command(cmd)                        
+                        run_command(cmd)
                         log.info(NAME, _('Mariadb is now installed.'))
+                        log.info(NAME, _('Please wait installing mariadb-client-10.0...'))
+                        cmd = "sudo apt-get install mariadb-client-10.0"
+                        run_command(cmd)
+
 
             except Exception:
                 log.clear(NAME)
@@ -158,7 +166,7 @@ def execute_db(sql = "", commit = False, test = False):
                         dbname += re.sub("[()',]","", str(msg[c])) + '\n'
                     log.info(NAME, _('Database type') + ': {}'.format(dbtype) + '\n' + _('Found databases') + ': \n{}'.format(dbname))
                 else:
-                    log.info(NAME, _('Not found') + '.')    
+                    log.info(NAME, _('Not found') + '.')
             
             cur.close()
             return -1 if msg is None else msg
@@ -167,6 +175,73 @@ def execute_db(sql = "", commit = False, test = False):
             log.error(NAME, datetime_string() + '\n' + _('Error connecting to MariaDB Platform') + ':\n{}'.format(e))
             return e
 
+def get_dump():
+    try:
+        filestamp = time.strftime('%Y%m%d-%H%M%S')
+        bkp_name = '{}_{}.sql'.format(plugin_options['database'], filestamp)
+        path = os.path.join(plugin_data_dir(), bkp_name)
+        import re
+        sanity_password = re.sub('["]','\\"', str(plugin_options['pass'])) # sanity in password if char is "  example:  123"456  -> 123\\"456
+        process = os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s" % (
+            plugin_options['host'],
+            plugin_options['port'],
+            plugin_options['user'],
+            sanity_password,
+            plugin_options['database'],
+            path)
+        )
+        preprocessed = process.read()
+        print(preprocessed)
+        process.close()
+        log.info(NAME, 'Database dumped to' + ' ' + bkp_name)
+        return True
+    except:
+        log.error(NAME, _('Database Connector') + ':\n' + traceback.format_exc())
+        pass
+        return False
+
+
+### Read all sql files in data folder ###
+def read_sql_folder():
+    try:
+        dir_name =  plugin_data_dir() + '/'
+        # Get list of all files only in the given directory
+        list_of_files = filter( lambda x: os.path.isfile(os.path.join(dir_name, x)), os.listdir(dir_name) )
+        # Sort list of files based on last modification time in ascending order
+        list_of_files = sorted( list_of_files, key = lambda x: os.path.getmtime(os.path.join(dir_name, x)))
+        # Along with last modification time of file
+        f = []
+        g = []
+
+        for file_name in list_of_files:
+            file_path = os.path.join(dir_name, file_name)
+            size = os.path.getsize(file_path)
+            f.append(file_name)
+            g.append(round(size, 2))
+
+        plugin_options.__setitem__('sql_name', f)
+        plugin_options.__setitem__('sql_size', g)
+
+    except Exception:
+        log.error(NAME, _('Database Connector') + ':\n' + traceback.format_exc())
+
+
+def read_file(filename):
+    """Read data from file."""
+
+    try:
+        with open(os.path.join(plugin_data_dir(), filename)) as logf:
+            return logf.read()
+    except IOError:
+        return []
+
+
+def download_sql_file(path, name):
+    content = mimetypes.guess_type(os.path.join(plugin_data_dir(), name)[0])
+    web.header('Access-Control-Allow-Origin', '*')
+    web.header('Content-type', content) 
+    web.header('Content-Disposition', 'attachment; filename="{}"'.format(name))
+    return read_file(path)               
 
 ################################################################################
 # Web pages:                                                                   #
@@ -178,7 +253,6 @@ class settings_page(ProtectedPage):
     def GET(self):
         global sender
         qdict  = web.input()
-
         test = get_input(qdict, 'test', False, lambda x: True)
 
         if sender is not None and test:
@@ -190,6 +264,43 @@ class settings_page(ProtectedPage):
     def POST(self):
         plugin_options.web_update(web.input())
         raise web.seeother(plugin_url(settings_page), True)
+
+
+class backup_page(ProtectedPage):
+    """Load an html page for backup"""
+
+    def GET(self):
+        global sender
+        qdict  = web.input()
+        backup = get_input(qdict, 'backup', False, lambda x: True)
+
+        log.clear(NAME)
+        if sender is not None and backup:
+            if get_dump():
+                log.info(NAME, datetime_string() + ': ' + _('Created database backup.'))
+            else:
+                log.error(NAME, datetime_string() + ': ' + _('Error in database backup.'))
+
+        if 'delete' in qdict and sender is not None:
+            delete = qdict['delete']
+            if len(plugin_options['sql_name']) > 0:
+                del_file = os.path.join(plugin_data_dir(), plugin_options['sql_name'][int(delete)] )
+                if os.path.isfile(del_file):
+                    os.remove(del_file)
+                    log.debug(NAME, datetime_string() + ': ' + _('Deleting file has sucesfully.'))
+                else:
+                    log.error(NAME, datetime_string() + ': ' + _('File for deleting not found!'))                    
+
+        if 'download' in qdict and sender is not None:
+            download = qdict['download']
+            if len(plugin_options['sql_name']) > 0:
+                down_name = plugin_options['sql_name'][int(download)]
+                down_path = os.path.join(plugin_data_dir(), down_name)
+                if os.path.isfile(down_path):
+                    download_sql_file(down_path, down_name)
+        
+        read_sql_folder()
+        return self.plugin_render.database_connector_backup(plugin_options, log.events(NAME))
 
 
 class help_page(ProtectedPage):
