755bf8168cf1b620d3a39a0f8e20024a0609ba4c
Martin Pihrt <martinpihrt@seznam.cz>
2024-08-14 12:06:44 +0200

    Add try except test to web

--- plugins/cli_control/__init__.py
+++ plugins/cli_control/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__author__ = u'Martin Pihrt'
+__author__ = 'Martin Pihrt'
 
 import json
 import time
@@ -21,15 +21,15 @@ from plugins import PluginOptions, plugin_url, plugin_data_dir
 from ospy.webpages import ProtectedPage
 
 NAME = 'CLI Control'         ### name for plugin in plugin manager ###
-MENU =  _(u'Package: CLI Control')
+MENU =  _('Package: CLI Control')
 LINK = 'settings_page'       ### link for page in plugin manager ###
 
 plugin_options = PluginOptions(
     NAME,
-    {u'use_control': False,
-     u'on':  [u"echo 'example start command for station 1'",u"",u"",u"",u"",u"",u"",u""],
-     u'off': [u"echo 'example stop command for station 1'",u"",u"",u"",u"",u"",u"",u""],
-     u'use_log': False  
+    {'use_control': False,
+     'on':  ["echo 'example start command for station 1'","","","","","","",""],
+     'off': ["echo 'example stop command for station 1'","","","","","","",""],
+     'use_log': False  
     }
 )
 
@@ -64,7 +64,7 @@ class Sender(Thread):
     def run(self):
         if plugin_options['use_control']:  # if plugin is enabled
             log.clear(NAME)
-            log.info(NAME, _(u'CLI Control is enabled.'))
+            log.info(NAME, _('CLI Control is enabled.'))
             try:
                 station_on = signal('station_on')
                 station_on.connect(on_station_on)
@@ -73,11 +73,11 @@ class Sender(Thread):
                 station_clear = signal('station_clear')
                 station_clear.connect(on_station_clear)
             except Exception:
-                log.error(NAME, _(u'CLI Control plug-in') + ':\n' + traceback.format_exc())
+                log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
                 pass
         else:
             log.clear(NAME)
-            log.info(NAME, _(u'CLI Control is disabled.'))
+            log.info(NAME, _('CLI Control is disabled.'))
 
 sender = None
 
@@ -111,50 +111,59 @@ def run_command(cmd):
             if plugin_options['use_log']:
                 update_log(cmd, output)
         except:
-            log.error(NAME, _(u'CLI Control plug-in') + ':\n' + traceback.format_exc())
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
             pass
 
 def on_station_on(name, **kw):
     """ Send CMD to ON when core program signals in station state."""
-    index = int(kw[u"txt"])
-    log.clear(NAME)
-    log.info(NAME, _(u'Station {} change to ON').format(index+1))
-    command = plugin_options['on'] 
-    data = command[index]
-    if data:
-        log.info(NAME, _(u'Im trying to send an ON command: {}').format(index+1))
-        run_command(data)
-    else:
-        log.info(NAME, _(u'No command is set for ON: {}').format(index+1))
-    return 
+    try:
+        index = int(kw["txt"])
+        log.clear(NAME)
+        log.info(NAME, _('Station {} change to ON').format(index+1))
+        command = plugin_options['on'] 
+        data = command[index]
+        if data:
+            log.info(NAME, _('Im trying to send an ON command: {}').format(index+1))
+            run_command(data)
+        else:
+            log.info(NAME, _('No command is set for ON: {}').format(index+1))
+    except:
+        log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+        pass
 
 def on_station_off(name, **kw):
     """ Send CMD to OFF when core program signals in station state."""
-    index = int(kw[u"txt"])    
-    log.clear(NAME)
-    log.info(NAME, _(u'Station {} change to OFF').format(index+1))
-    command = plugin_options['off'] 
-    data = command[index]
-    if data:
-        log.info(NAME, _(u'Im trying to send an OFF command: {}').format(index+1))
-        run_command(data)
-    else:
-        log.info(NAME, _(u'No command is set for OFF: {}').format(index+1))
-    return
+    try:
+        index = int(kw["txt"])
+        log.clear(NAME)
+        log.info(NAME, _('Station {} change to OFF').format(index+1))
+        command = plugin_options['off']
+        data = command[index]
+        if data:
+            log.info(NAME, _('Im trying to send an OFF command: {}').format(index+1))
+            run_command(data)
+        else:
+            log.info(NAME, _('No command is set for OFF: {}').format(index+1))
+    except:
+        log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+        pass   
 
 def on_station_clear(name, **kw):
     """ Send all CMD to OFF when core program signals in station state."""
-    log.clear(NAME)
-    log.info(NAME, _(u'All station change to OFF'))
-    for station in stations.get():
-        command = plugin_options['off']
-        data = command[station.index]
-        if data:
-            log.info(NAME, _(u'Im trying to send an OFF command: {}').format(station.index+1))
-            run_command(data)
-        else:
-            log.info(NAME, _(u'No command is set for OFF: {}').format(station.index+1))
-    return
+    try:
+        log.clear(NAME)
+        log.info(NAME, _('All station change to OFF'))
+        for station in stations.get():
+            command = plugin_options['off']
+            data = command[station.index]
+            if data:
+                log.info(NAME, _('Im trying to send an OFF command: {}').format(station.index+1))
+                run_command(data)
+            else:
+                log.info(NAME, _('No command is set for OFF: {}').format(station.index+1))
+    except:
+        log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+        pass
 
 def read_log():
     """Read log data from json file."""
@@ -166,27 +175,36 @@ def read_log():
 
 def write_log(json_data):
     """Write data to log json file."""
-    with open(os.path.join(plugin_data_dir(), 'log.json'), 'w') as outfile:
-        json.dump(json_data, outfile)
+    try:
+        with open(os.path.join(plugin_data_dir(), 'log.json'), 'w') as outfile:
+            json.dump(json_data, outfile)
+    except:
+        log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+        pass
 
 def update_log(cmd, status):
     """Update data in json files."""
     try:
-        log_data = read_log()
-    except:   
-        write_log([])
-        log_data = read_log()
+        try:
+            log_data = read_log()
+        except:   
+            write_log([])
+            log_data = read_log()
 
-    from datetime import datetime 
+        from datetime import datetime 
 
-    data = {}
-    data['cmd'] = cmd
-    data['status'] = status
-    data['datetime'] = datetime_string()
+        data = {}
+        data['cmd'] = cmd
+        data['status'] = status
+        data['datetime'] = datetime_string()
 
-    log_data.insert(0, data)
-    write_log(log_data)
-    log.info(NAME, _(u'Saving to log files OK'))
+        log_data.insert(0, data)
+        write_log(log_data)
+        log.info(NAME, _('Saving to log files OK'))
+    except:
+        log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+        pass
+
 
 ################################################################################
 # Web pages:                                                                   #
@@ -196,44 +214,50 @@ class settings_page(ProtectedPage):
     """Load an html page for entering adjustments."""
 
     def GET(self):
-        global sender
-        qdict = web.input()
-        delete = helpers.get_input(qdict, 'delete', False, lambda x: True)
-        show = helpers.get_input(qdict, 'show', False, lambda x: True)
-        state = helpers.get_input(qdict, 'state', False, lambda x: True)
- 
-        if sender is not None and 'test' in qdict:
-            test = qdict['test']
-            if state:
-                log.clear(NAME)
-                log.info(NAME, _(u'Test CMD: {} ON.').format(int(test)+1))
-                command = plugin_options['on'] 
-                data = command[int(test)]
-                if data:
-                    run_command(data)
-            else:
-                log.clear(NAME)
-                log.info(NAME, _(u'Test CMD: {} OFF.').format(int(test)+1))
-                command = plugin_options['off'] 
-                data = command[int(test)]
-                if data:
-                    run_command(data)
-
-        if sender is not None and delete:
-            write_log([])
-            log.info(NAME, _(u'Deleted all log files OK'))
-            raise web.seeother(plugin_url(settings_page), True)
-
-        if sender is not None and show:
-            raise web.seeother(plugin_url(log_page), True)
-
-        return self.plugin_render.cli_control(plugin_options, log.events(NAME))
-
-    def POST(self):
-        qdict = web.input()
-        
         try:
-           ### add code to update commands ###
+            global sender
+            qdict = web.input()
+            delete = helpers.get_input(qdict, 'delete', False, lambda x: True)
+            show = helpers.get_input(qdict, 'show', False, lambda x: True)
+            state = helpers.get_input(qdict, 'state', False, lambda x: True)
+ 
+            if sender is not None and 'test' in qdict:
+                test = qdict['test']
+                if state:
+                    log.clear(NAME)
+                    log.info(NAME, _('Test CMD: {} ON.').format(int(test)+1))
+                    command = plugin_options['on'] 
+                    data = command[int(test)]
+                    if data:
+                        run_command(data)
+                else:
+                    log.clear(NAME)
+                    log.info(NAME, _('Test CMD: {} OFF.').format(int(test)+1))
+                    command = plugin_options['off'] 
+                    data = command[int(test)]
+                    if data:
+                        run_command(data)
+
+            if sender is not None and delete:
+                write_log([])
+                log.info(NAME, _('Deleted all log files OK'))
+                raise web.seeother(plugin_url(settings_page), True)
+
+            if sender is not None and show:
+                raise web.seeother(plugin_url(log_page), True)
+
+            return self.plugin_render.cli_control(plugin_options, log.events(NAME))
+
+        except:
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+            msg = _('An internal error was found in the system, see the error log for more information. The error is in part:') + ' '
+            msg += _('cli_control -> settings_page GET')
+            return self.core_render.notice('/', msg)
+
+    def POST(self):    
+        try:
+            qdict = web.input()
+
             if 'use_control' in qdict:
                 if qdict['use_control']=='on':
                     plugin_options.__setitem__('use_control', True)
@@ -260,22 +284,37 @@ class settings_page(ProtectedPage):
             if sender is not None:
                 sender.update()
 
-        except Exception:
-            log.error(NAME, _(u'CLI Control plug-in') + ':\n' + traceback.format_exc())
+            raise web.seeother(plugin_url(settings_page), True)
 
-        raise web.seeother(plugin_url(settings_page), True)
+        except:
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+            msg = _('An internal error was found in the system, see the error log for more information. The error is in part:') + ' '
+            msg += _('cli_control -> settings_page POST')
+            return self.core_render.notice('/', msg)
 
 class help_page(ProtectedPage):
     """Load an html page for help"""
 
     def GET(self):
-        return self.plugin_render.cli_control_help()
+        try:
+            return self.plugin_render.cli_control_help()
+        except:
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+            msg = _('An internal error was found in the system, see the error log for more information. The error is in part:') + ' '
+            msg += _('cli_control -> help_page GET')
+            return self.core_render.notice('/', msg)
 
 class log_page(ProtectedPage):
     """Load an html page for help"""
 
     def GET(self):
-        return self.plugin_render.cli_control_log(read_log())
+        try:
+            return self.plugin_render.cli_control_log(read_log())
+        except:
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+            msg = _('An internal error was found in the system, see the error log for more information. The error is in part:') + ' '
+            msg += _('cli_control -> log_page GET')
+            return self.core_render.notice('/', msg)
 
 class settings_json(ProtectedPage): 
     """Returns plugin settings in JSON format."""
@@ -283,7 +322,11 @@ class settings_json(ProtectedPage):
     def GET(self):
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Content-Type', 'application/json')
-        return json.dumps(plugin_options)
+        try:
+            return json.dumps(plugin_options)
+        except:
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+            return {}
 
 class log_json(ProtectedPage):
     """Returns data in JSON format."""
@@ -291,23 +334,33 @@ class log_json(ProtectedPage):
     def GET(self):
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Content-Type', 'application/json')
-        return json.dumps(read_log())
+        try:
+            return json.dumps(read_log())
+        except:
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+            return {}
 
 class log_csv(ProtectedPage):
     """Simple Log API"""
 
     def GET(self):
-        data = "Date/Time; Command; State \n"
-        log_file = read_log()
-        for interval in log_file:
-            data += '; '.join([
-                interval['datetime'],
-                u'{}'.format(interval['cmd']),
-                u'{}'.format(interval['status']),
-            ]) + '\n'
+        data = ""
+        try:
+            data = "Date/Time; Command; State \n"
+            log_file = read_log()
+            for interval in log_file:
+                data += '; '.join([
+                    interval['datetime'],
+                    '{}'.format(interval['cmd']),
+                    '{}'.format(interval['status']),
+                ]) + '\n'
 
-        content = mimetypes.guess_type(os.path.join(plugin_data_dir(), 'log.json')[0])
-        web.header('Access-Control-Allow-Origin', '*')
-        web.header('Content-type', content) 
-        web.header('Content-Disposition', 'attachment; filename="log.csv"')
+            content = mimetypes.guess_type(os.path.join(plugin_data_dir(), 'log.json')[0])
+            web.header('Access-Control-Allow-Origin', '*')
+            web.header('Content-type', content) 
+            web.header('Content-Disposition', 'attachment; filename="log.csv"')
+        except:
+            log.error(NAME, _('CLI Control plug-in') + ':\n' + traceback.format_exc())
+            pass
+
         return data
