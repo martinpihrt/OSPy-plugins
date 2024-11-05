adafac574bae517e940c8cbe09d69e76d728a2cd
Martin Pihrt <martinpihrt@seznam.cz>
2024-11-05 19:14:02 +0100

    pre test buttons

--- plugins/current_loop_tanks_monitor/__init__.py
+++ plugins/current_loop_tanks_monitor/__init__.py
@@ -114,7 +114,8 @@ class Sender(Thread):
 
     def run(self):
         tank_mon = None
-        tempText = ""
+        last_millis = int(round(time.time() * 1000))                            # timer for saving log
+        last_millis_2 = int(round(time.time() * 1000))                          # timer for periodic measuring
 
         if plugin_options['use_footer']:
             tank_mon = showInFooter()                                           # instantiate class to enable data in footer
@@ -123,25 +124,36 @@ class Sender(Thread):
 
         while not self._stop_event.is_set():
             try:
-                log.clear(NAME)
-                
-#                scan_i2c()
-                get_data()
+                millis = int(round(time.time() * 1000))
+    
+                ### periodically measuring after 5 seconds
+                if (millis - last_millis_2) >= 5000:
+                    last_millis_2 = millis
+                    log.clear(NAME)
+                    get_data()
+#                   scan_i2c()
+
+                ### periodically logging (minute interval)
+                if plugin_options['en_log'] or plugin_options['en_sql_log']:
+                    interval = (plugin_options['log_interval'] * 60000)
+                    if (millis - last_millis) >= interval:
+                       last_millis = millis
+                       update_log()                
 
                 if plugin_options['use_footer']:
-                    if tank_mon is not None:  
-                        tempText = '{} {} %, {} {} %, {} {} %, {} {} %,'.format(
-                            tanks['label'][0],
-                            tanks['levelPercent'][0],
-                            tanks['label'][1],
-                            tanks['levelPercent'][1],
-                            tanks['label'][2],
-                            tanks['levelPercent'][2],
-                            tanks['label'][3],
-                            tanks['levelPercent'][3]
-                            )
-                        tank_mon.val = tempText.encode('utf8').decode('utf8')   # value on footer
-                self._sleep(5)
+                    if tank_mon is not None:
+                        tempText = ""
+                        if plugin_options['en_tank1']: 
+                            tempText += '{} {} % '.format(tanks['label'][0], tanks['levelPercent'][0])
+                        if plugin_options['en_tank2']: 
+                            tempText += '{} {} % '.format(tanks['label'][1], tanks['levelPercent'][1])
+                        if plugin_options['en_tank3']: 
+                            tempText += '{} {} % '.format(tanks['label'][2], tanks['levelPercent'][2])
+                        if plugin_options['en_tank4']: 
+                            tempText += '{} {} % '.format(tanks['label'][3], tanks['levelPercent'][3])                        
+                        tank_mon.val = tempText.encode('utf8').decode('utf8')
+                
+                self._sleep(1)
 
             except Exception:
                 log.clear(NAME)
@@ -291,18 +303,21 @@ def get_data():
             #log.error(NAME, _('I/O error for channel {}: {}.').format(channel, ioe))
 
     if IO_error:    
-            log.error(NAME, _('I/O error.'))
+        log.error(NAME, _('Error: I/O.'))
 
     if VAL_error:
-            log.error(NAME, _('Any Error on ADC channel.'))
-           
+        log.error(NAME, _('Error: ADC value.'))
+
+    if not IO_error and not VAL_error:
+        log.info(NAME, _('No problems (measurement works as it should).'))
+
 
 def scan_i2c():
     bus = None
     try:
         bus = smbus.SMBus(1)
     except FileNotFoundError:
-       log.error(NAME, _('Error: the I2C bus is not available.'))
+       log.error(NAME, _('Error: I2C bus is not available.'))
        return []
 
     devices = []
@@ -356,7 +371,7 @@ def create_default_graph():
        {"station": _('Tank 4'), "balances": {}}
     ]
     write_graph_log(graph_data)
-    log.info(NAME, _('Create default graph json file.'))
+#    log.debug(NAME, _('Create default graph json file.'))
 
 
 def write_log(json_data):
@@ -490,7 +505,7 @@ def update_log():
         except:
             create_default_graph()
             graph_data = read_graph_log()
-            log.debug(NAME, _('Create default graph json file.'))
+#            log.debug(NAME, _('Create default graph json file.'))
 
         timestamp = int(time.time())
 
@@ -512,7 +527,7 @@ def update_log():
             tmp3.update({epoch: tank4})
 
             write_graph_log(graph_data)
-            log.info(NAME, _('Saving to graph log files.'))
+#            log.info(NAME, _('Saving to graph log files.'))
         except:
             create_default_graph()
 
@@ -525,7 +540,7 @@ def update_log():
             # next insert data to table tankmonitor
             sql = "INSERT INTO `currentmonitor` (`tank1`, `tank2`, `tank3`, `tank4`) VALUES ('%s','%s','%s','%s')" % (tanks['levelPercent'][0],tanks['levelPercent'][1],tanks['levelPercent'][2],tanks['levelPercent'][3])
             execute_db(sql, test=False, commit=True)  # yes commit inserted data
-            log.info(NAME, _('Saving to SQL database.'))
+#            log.debug(NAME, _('Saving to SQL database.'))
         except:
             log.error(NAME, _('Current Loop Tanks Monitor plug-in') + ':\n' + traceback.format_exc())
             pass
@@ -538,12 +553,32 @@ class settings_page(ProtectedPage):
     """Load an html page for tank."""
 
     def GET(self):
+        global sender
+        qdict  = web.input()                
+        log_now = helpers.get_input(qdict, 'log_now', False, lambda x: True)
+        delfilter = helpers.get_input(qdict, 'delfilter', False, lambda x: True)        
+        
+        if sender is not None and log_now:
+            update_log()
+
+        if sender is not None and 'dt_from' in qdict and 'dt_to' in qdict:
+            dt_from = qdict['dt_from']
+            dt_to = qdict['dt_to']
+            plugin_options.__setitem__('dt_from', dt_from) #__setitem__(self, key, value)
+            plugin_options.__setitem__('dt_to', dt_to)     #__setitem__(self, key, value)
+
+        if sender is not None and delfilter:
+            from datetime import datetime, timedelta
+            dt_now = (datetime.today() + timedelta(days=1)).date()
+            plugin_options.__setitem__('dt_from', "2020-01-01T00:00")
+            plugin_options.__setitem__('dt_to', "{}T00:00".format(dt_now))                    
+
         return self.plugin_render.current_loop_tanks_monitor(plugin_options)
 
     def POST(self):
         global sender
         qdict  = web.input()
-
+        print(qdict)
         # switch 1-4 on plugin homepage in tank (on-off for tanks)
         for i in range(1, 4):
             if sender is not None:
@@ -552,8 +587,7 @@ class settings_page(ProtectedPage):
                 else:
                     plugin_options.__setitem__('en_tank{}'.format(i), False)
 
-        log.debug(NAME, _('Options has updated.'))
-        return 'OK'           
+        return self.plugin_render.current_loop_tanks_monitor(plugin_options)           
 
 
 class help_page(ProtectedPage):
@@ -567,7 +601,48 @@ class graph_page(ProtectedPage):
     """Load an html page for graph"""
 
     def GET(self):
-        return self.plugin_render.current_loop_tanks_monitor_graph(plugin_options)
+        data = []
+        try:
+            from datetime import datetime
+
+            dt_from = datetime.strptime(plugin_options['dt_from'], '%Y-%m-%dT%H:%M') # from
+            dt_to   = datetime.strptime(plugin_options['dt_to'], '%Y-%m-%dT%H:%M')   # to
+
+            epoch_time = datetime(1970, 1, 1)
+
+            log_start = int((dt_from - epoch_time).total_seconds())
+            log_end = int((dt_to - epoch_time).total_seconds())
+
+            json_data = [{}]
+
+            try:
+                if plugin_options['type_log'] == 0:
+                    json_data = read_graph_log()
+                if plugin_options['type_log'] == 1:
+                    json_data = read_graph_sql_log()
+            except:
+                log.error(NAME, _('Current Loop Tanks Monitor plug-in') + ':\n' + traceback.format_exc())
+                pass
+
+            if len(json_data) > 0:  
+                for i in range(0, 4):                                              # 0=tank1, 1=tank2, 2=tank3, 3=tank4
+                    temp_balances = {}
+                    for key in json_data[i]['balances']:
+                        try:
+                            find_key = int(key.encode('utf8'))                     # key is in unicode ex: u'1601347000' -> find_key is int number
+                        except:
+                            find_key = key      
+                        if find_key >= log_start and find_key <= log_end:          # timestamp interval from <-> to
+                            temp_balances[key] = json_data[i]['balances'][key]    
+                    data.append({ 'station': json_data[i]['station'], 'balances': temp_balances })
+
+        except:
+            log.error(NAME, _('Current Loop Tanks Monitor plug-in') + ':\n' + traceback.format_exc())
+            pass
+
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+        return json.dumps(data)
 
 
 class log_page(ProtectedPage):
@@ -584,7 +659,7 @@ class log_page(ProtectedPage):
                 from plugins.database_connector import execute_db
                 sql = "DROP TABLE IF EXISTS `currentmonitor`"
                 execute_db(sql, test=False, commit=False)  
-                log.info(NAME, _('Deleting the currentmonitor table from the database.'))
+#                log.info(NAME, _('Deleting the currentmonitor table from the database.'))
             except:
                 log.error(NAME, _('Current Loop Tanks Monitor plug-in') + ':\n' + traceback.format_exc())
                 pass
@@ -610,7 +685,7 @@ class setup_page(ProtectedPage):
                 from plugins.database_connector import execute_db
                 sql = "DROP TABLE IF EXISTS `currentmonitor`"
                 execute_db(sql, test=False, commit=False)  
-                log.info(NAME, _('Deleting the currentmonitor table from the database.'))
+#                log.debug(NAME, _('Deleting the currentmonitor table from the database.'))
             except:
                 log.error(NAME, _('Current Loop Tanks Monitor plug-in') + ':\n' + traceback.format_exc())
                 pass
@@ -625,7 +700,7 @@ class setup_page(ProtectedPage):
         if sender is not None:
             sender.update()
 
-        log.debug(NAME, _('Options has updated.'))
+#        log.debug(NAME, _('Options has updated.'))
         raise web.seeother(plugin_url(setup_page), True)
 
 
